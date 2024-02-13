74d9296a27a5d99133f0e2c11677267cee3350aa
= <barkatullahkhan120136@gmail.com>
2024-02-13 09:46:24 -0800

    render html file via express

--- src/Index.js
+++ src/Index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const Port = 8080;
+const pathName = path.join(__dirname, "../public");
+
+app.use(express.static(pathName));
 
 app.get("/", (req, res) => {
   res.send("welcome to my home page");
